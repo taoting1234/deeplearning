524e2ea278f6e5f1b2c125998f8cb11890cbadcf
taoting <335354289@qq.com>
2019-05-14 19:56:36 +0800

    优化

--- text_sentiment_classification/model.py
+++ text_sentiment_classification/model.py
@@ -3,12 +3,18 @@ from tensorflow import keras
 from text_sentiment_classification.pretreatment import load_data
 
 train_epochs = 10
-batch_size = 100
+batch_size = 10
 test_batch_size = 100
 learning_rate = 0.001
-display_step = 10
 
-(x_train_data, y_train_data), (x_test_data, y_test_data) = load_data('data/train.csv')
+test_ratio = 0.1
+min_count = 5
+vector_size = 100
+max_len = 100
+input_length = 100
+
+(x_train_data, y_train_data), (x_test_data, y_test_data), n_symbols, embedding_weights = \
+    load_data('data/train.csv', test_ratio, min_count, vector_size, max_len)
 
 train_dataset = tf.data.Dataset.from_tensor_slices((x_train_data, y_train_data)) \
     .map(lambda a, b: (a, tf.one_hot(b, 2))) \
@@ -23,14 +29,20 @@ train_next_element = train_iterator.get_next()
 test_iterator = test_dataset.make_initializable_iterator()
 test_next_element = test_iterator.get_next()
 
-model = keras.Sequential()
-model.add(keras.layers.Bidirectional(keras.layers.LSTM(128, input_shape=(50, 100))))
+model = keras.Sequential()  # or Graph or whatever
+model.add(keras.layers.Embedding(output_dim=vector_size,
+                                 input_dim=n_symbols,
+                                 mask_zero=True,
+                                 weights=[embedding_weights],
+                                 input_length=input_length))  # Adding Input Length
+model.add(keras.layers.LSTM(128, activation='tanh'))
 model.add(keras.layers.Dropout(0.5))
-model.add(keras.layers.Dense(2, activation='relu'))
+model.add(keras.layers.Dense(2, activation='softmax'))  # Dense=>全连接层,输出维度=3
+model.add(keras.layers.Activation('softmax'))
 
 model.summary()
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(train_dataset, epochs=train_epochs, steps_per_epoch=x_train_data.shape[0] // batch_size,
           validation_data=test_dataset, validation_steps=10)
 
--- text_sentiment_classification/pretreatment.py
+++ text_sentiment_classification/pretreatment.py
@@ -1,8 +1,9 @@
+import nltk
 import pandas as pd
 import numpy as np
-import json
 import re
 
+from gensim.corpora import Dictionary
 from gensim.models import word2vec
 from tensorflow import keras
 
@@ -11,56 +12,63 @@ def segmentation(sentence):
     return re.split(r'\W+', sentence)
 
 
-def sentence_to_vector(data, word_vector, max_len=50):
-    vector = []
-    for i in data:
-        tmp = []
-        for j in segmentation(i):
-            try:
-                tmp.append(word_vector.wv.get_vector(j))
-            except KeyError:
-                tmp.append(np.zeros(100))
-        tmp = np.array(tmp)
-        vector.append(tmp)
-    vector = np.array(vector)
-    return keras.preprocessing.sequence.pad_sequences(vector, maxlen=max_len, dtype='float64')
-
-
-def load_data(filename, test_ratio=0.1):
+def load_data(filename, test_ratio=0.1, min_count=5, vector_size=100, max_len=50):
     df = pd.read_csv(filename, lineterminator='\n')
     data = np.array(df.values)
     train_data_num = int(data.shape[0] * (1 - test_ratio))
     x_train_data = data[0:train_data_num, 1]
     y_train_data = data[0:train_data_num, 2]
+
     x_test_data = data[train_data_num:, 1]
     y_test_data = data[train_data_num:, 2]
 
-    word_vector = create_word2vec_model(filename)
+    model = create_word2vec_model([segmentation(i) for i in x_train_data], min_count, vector_size)
+    index_dict, word_vectors, x_train_data = create_dictionaries(model, [segmentation(i) for i in x_train_data],
+                                                                 max_len)
+    _, _, x_test_data = create_dictionaries(model, [segmentation(i) for i in x_test_data], max_len)
 
-    x_train_data = sentence_to_vector(x_train_data, word_vector)
-    x_test_data = sentence_to_vector(x_test_data, word_vector)
+    n_symbols = len(index_dict) + 1  # 所有单词的索引数，频数小于10的词语索引为0，所以加1
+    embedding_weights = np.zeros((n_symbols, vector_size))  # 初始化 索引为0的词语，词向量全为0
+    for word, index in index_dict.items():  # 从索引为1的词语开始，对每个词语对应其词向量
+        embedding_weights[index, :] = word_vectors[word]
 
     y_train_data = np.where(y_train_data == 'Positive', 1, 0)
     y_test_data = np.where(y_test_data == 'Positive', 1, 0)
 
-    return (x_train_data, y_train_data), (x_test_data, y_test_data)
+    return (x_train_data, y_train_data), (x_test_data, y_test_data), n_symbols, embedding_weights
 
 
-def create_word2vec_model(filename):
-    df = pd.read_csv(filename, lineterminator='\n')
-    data = np.array(df.values)
-    sentences = [segmentation(i) for i in data[:, 1]]
-    model = word2vec.Word2Vec(sentences, min_count=5)
+def create_dictionaries(model, combined, max_len=50):
+    ''' Function does are number of Jobs:
+        1- Creates a word to index mapping
+        2- Creates a word to vector mapping
+        3- Transforms the Training and Testing Dictionaries
+    '''
+    gensim_dict = Dictionary()
+    gensim_dict.doc2bow(model.wv.vocab.keys(), allow_update=True)
+    #  freqxiao10->0 所以k+1
+    w2indx = {v: k + 1 for k, v in gensim_dict.items()}  # 所有频数超过10的词语的索引,(k->v)=>(v->k)
+    w2vec = {word: model.wv[word] for word in w2indx.keys()}  # 所有频数超过10的词语的词向量, (word->model(word))
+
+    data = []
+    for sentence in combined:
+        new_txt = []
+        for word in sentence:
+            try:
+                new_txt.append(w2indx[word])
+            except:
+                new_txt.append(0)  # freqxiao10->0
+        data.append(new_txt)
+
+    combined = keras.preprocessing.sequence.pad_sequences(data, maxlen=max_len)
+    return w2indx, w2vec, combined
+
+
+def create_word2vec_model(combined, min_count=5, vector_size=100):
+    model = word2vec.Word2Vec(combined, min_count=min_count, size=vector_size)
     return model
-
-
-def save_word2vec_model(model, filename='word2vec.model'):
-    model.save(filename)
-
-
-def load_word2vec_model(filename):
-    return word2vec.Word2Vec.load(filename)
 
 
 if __name__ == '__main__':
     load_data('data/train.csv')
+    pass
